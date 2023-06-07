7d168f1d8aa35200a1f71e5d231dd9bbe8a7c1a5
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-06-07 19:52:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-// Redux
+// Redux store
 import { persistor, store } from './redux/store';
 // Components
 import { App } from 'components/App/App';
